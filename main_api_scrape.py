9346f7847e1539b81e00fd574516d13744c6bdda
Nunuvin <zloyzig@yandex.ru>
2021-10-11 18:43:47 -0600

    delay + save_to_file_func

--- main_api_scrape.py
+++ main_api_scrape.py
@@ -2,6 +2,7 @@ import requests
 import json
 from dotenv import load_dotenv
 import os
+import time
 
 load_dotenv()
 envBear = {}
@@ -30,13 +31,18 @@ saveFiles = {
 #             envBear[l[0]] = l[2]
 #         #print(envBear)
 
+def save_json_to_file(reqData):
+    with open(saveFiles["giveawayTweets"], 'w') as f:
+                json.dump(reqData, f)
+            exit()
+
 def scrape_giveaways():
     '''
     Get those sweet giveaways
     '''
     global saveFiles
 
-    tweetsByHashtag = "https://api.twitter.com/2/tweets/search/recent?query=%23crypto%20%23giveaway%20(tag%20OR%20comment)%20-is%3Aretweet%20-is%3Areply%20-is%3Aquote&tweet.fields=conversation_id,in_reply_to_user_id,author_id,referenced_tweets,source,text,id,public_metrics&expansions=author_id,entities.mentions.username,in_reply_to_user_id,referenced_tweets.id,referenced_tweets.id.author_id&max_results=10"
+    tweetsByHashtag = "https://api.twitter.com/2/tweets/search/recent?query=%23crypto%20%23giveaway%20(tag%20OR%20comment)%20-is%3Aretweet%20-is%3Areply%20-is%3Aquote&tweet.fields=conversation_id,in_reply_to_user_id,author_id,referenced_tweets,source,text,id,public_metrics&expansions=author_id,entities.mentions.username,in_reply_to_user_id,referenced_tweets.id,referenced_tweets.id.author_id&max_results=100"
 
     # nasa_tweets = 'https://api.twitter.com/1.1/search/tweets.json?q=crypto%20giveaway&result_type=popular&since_id=1417454251299819520&count=15'
     # nasa_tweets2 = "https://api.twitter.com/1.1/search/tweets.json?max_id=1446475489359585283&q=nasa&include_entities=1&result_type=popular"
@@ -58,9 +64,7 @@ def scrape_giveaways():
             print (nxtToken)
             print("issue at: ",i,"Error:\n")
             print(e)
-            with open(saveFiles["giveawayTweets"], 'w') as f:
-                json.dump(reqData, f)
-            exit()
+            save_json_to_file(reqData)
 
         #print (nxtToken)
 
@@ -69,10 +73,10 @@ def scrape_giveaways():
         reqData.extend(reqJson["data"])
         if i % 5 == 0:
             print("at: " + str(i))
+        time.sleep(5)
 
     # write to file
-    with open(saveFiles["giveawayTweets"], 'w') as f:
-        json.dump(reqData, f)
+    save_json_to_file(reqData)
 
 def main():
     #load_bearer_token() #load env vals
