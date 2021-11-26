9a953d4dd89036cb9e2cc9e11fd4dced9e546bb8
Stam743 <spencertam@rocketmail.com>
2021-11-26 15:58:50 -0700

    moved hashtags into one file

--- sortByCommunities.py
+++ sortByCommunities.py
@@ -13,23 +13,42 @@ out = {}
 giveaways = json.load(f)
 
 def findCommonHashtags():
-    with open('./Outputs/Community_Hashtags.json', 'w') as outfile:
-        for index, row in df.iterrows():
-            #print(row)
-            try:
-                text = giveaways[row['Label']]['text']
-                hashtag_list = re.findall("#[A-z0-9]+", text)
-                #print(hashtag_list)
-                
-                if row['modularity_class'] in out:
-                    out[row['modularity_class']] = out[row['modularity_class']] + hashtag_list
-                else:
-                    out[row['modularity_class']] = hashtag_list
-                #print(out)
-            except KeyError:
-                pass
+    for index, row in df.iterrows():
+        #print(row)
+        try:
+            text = giveaways[row['Label']]['text']
+            hashtag_list = re.findall("#[A-z0-9]+", text)
+            #print(hashtag_list)
+            
+            
+            if row['modularity_class'] in out:
+                out[row['modularity_class']] = out[row['modularity_class']] + hashtag_list
+            else:
+                out[row['modularity_class']] = hashtag_list
+            #print(out)
+        except KeyError:
+            pass
+       
+        
 
-        json_object = json.dumps(out)
+def countHashtags():
+    with open('./Outputs/Community_Hashtags.json', 'w') as outfile:
+        hashtagCount = {}
+        for Community, hashtags in out.items():
+            #print(Community)
+            if Community == '1':
+                #print(Community)
+                for hashtag in hashtags:
+                    if hashtag in hashtagCount:
+                        count = hashtagCount[hashtag] + 1
+                        #print(count)
+                        hashtagCount.update({hashtag: count})
+                        #print(hashtagCount)
+                    else:
+                        hashtagCount[hashtag] = 1
+        json_object = json.dumps(hashtagCount)
         outfile.write(json_object)
+    #print(hashtagCount)
     
 findCommonHashtags()
+countHashtags()
