5fdfdb9370ca5cd4f53bc31713eedd10ea2e6b97
Nunuvin <zloyzig@yandex.ru>
2021-12-02 20:35:16 -0700

    delta time between top tweets

--- Analysis/Null Model/NullModelAnalysis.py
+++ Analysis/Null Model/NullModelAnalysis.py
@@ -24,29 +24,29 @@ G = [G.subgraph(c).copy()
 def computeVariables(a):
     global path_total, clustering_total, G
 
-    NUM_EDGES = 15
+    NUM_EDGES = 155416
 
     G1 = nx.Graph(G)
-
     start_time = time.time()
     nx.double_edge_swap(G1, nswap=NUM_EDGES, max_tries=NUM_EDGES*10)
 
-    avg_path = nx.average_shortest_path_length(G1)
+    avg_path = nx.average_shortest_path_length(G1, weight='edge_attr')
     clustering = nx.average_clustering(G1, weight='edge_attr')
 
     
  #   path_total += avg_path
  #   clustering_total += clustering
-   
+    print("Process died: ", a)
     return (avg_path, clustering)
 #    print("--- %s seconds ---" % (time.time() - start_time))
 
 if __name__ == '__main__':
     simCount = 100
-    sol = [1 for i in range(0, simCount)]
+    sol = [i for i in range(0, simCount)]
     arr = []
-    with Pool(32) as p:
+    with Pool(24) as p:
         arr = p.map(computeVariables, sol)
+
     path_total = sum(p[0] for p in arr) / simCount
     clustering_total = sum(p[1] for p in arr) / simCount
     print("path_total: ",  path_total)
